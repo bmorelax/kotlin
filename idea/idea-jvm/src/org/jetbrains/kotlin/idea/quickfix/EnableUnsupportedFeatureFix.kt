1c0addb1f5ee882b6030539bad63497b10822128
Andrey Uskov <Andrey.Uskov@jetbrains.com>
2018-12-24 20:55:40 +0300

    Fix EA-129025 KNPE during enable unsupported feature

--- idea/idea-jvm/src/org/jetbrains/kotlin/idea/quickfix/EnableUnsupportedFeatureFix.kt
+++ idea/idea-jvm/src/org/jetbrains/kotlin/idea/quickfix/EnableUnsupportedFeatureFix.kt
@@ -81,7 +81,7 @@ sealed class EnableUnsupportedFeatureFix(
             val targetVersion = feature.sinceVersion!!
 
             KotlinCommonCompilerArgumentsHolder.getInstance(project).update {
-                val parsedApiVersion = ApiVersion.parse(apiVersion!!)
+                val parsedApiVersion = if (apiVersion != null) ApiVersion.parse(apiVersion!!) else null
                 if (parsedApiVersion != null && feature.sinceApiVersion > parsedApiVersion) {
                     if (!checkUpdateRuntime(project, feature.sinceApiVersion)) return@update
                     apiVersion = feature.sinceApiVersion.versionString
